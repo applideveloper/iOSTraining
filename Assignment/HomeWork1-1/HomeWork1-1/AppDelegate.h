d627c5120c54c805ae667043967300a6c5455280
Atsushi Araki <araki_atsushi@cyberagent.co.jp>
2014-09-16 16:18:49 +0900

    Initial Commit

---
+++ Assignment/HomeWork1-1/HomeWork1-1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomeWork1-1
+//
+//  Created by 荒木 敦 on 2014/09/16.
+//  Copyright (c) 2014年 WishMatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Assignment/HomeWork1-1/HomeWork1-1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HomeWork1-1
+//
+//  Created by 荒木 敦 on 2014/09/16.
+//  Copyright (c) 2014年 WishMatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
